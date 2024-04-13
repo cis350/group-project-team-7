4fbb7df9f030e3787c153e4447630720d20492dc
Ethan Ma <ethma727@gmail.com>
2024-04-13 10:45:25 -0400

    added react version to lint file

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -20,6 +20,7 @@ export default [
               myCustomGlobal: "readonly"
           }
       },
+      settings: { react: { version: "detect" } },
     },
     pluginReactConfig,
     pluginJs.configs.recommended,
