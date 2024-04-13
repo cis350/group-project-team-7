920e84ca552ed2af713a90db944e577104f15b8a
Ethan Ma <ethma727@gmail.com>
2024-04-12 20:41:14 -0400

    updated, directly to login page with choice of signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
+// import Home from './components/Home';
 import Login from './components/Login'
 import Signup from './components/Signup'
-import Profile from './components/Profile'
 
 // Define the pages of our site
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<div>Not found.</div>} />
       </Routes>
     </div>
