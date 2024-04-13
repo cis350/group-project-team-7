a193ebe1832de2a45c88c24a842e771318b26849
Ethan Ma <ethma727@gmail.com>
2024-04-13 10:49:41 -0400

    forgot to add react lol

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-// import Home from './components/Home';
 import Login from './components/Login'
 import Signup from './components/Signup'
 
