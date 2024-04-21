c2670d8330460809120579fb8648602db20584c9
Ethan Ma <ethma727@gmail.com>
2024-04-21 16:49:14 -0400

    init form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
 import Signup from './components/Signup'
+import Form from './components/Form'
 
 // Define the pages of our site
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/form" element={<Form />} />
         <Route path="*" element={<div>Not found.</div>} />
       </Routes>
     </div>
